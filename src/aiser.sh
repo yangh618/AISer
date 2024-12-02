49f919a055bf9012477cd42de0a14046d3572f68
Yang Huang <hyhy123.cn@gmail.com>
2024-12-02 18:11:55 +0800

    Added some comments.

--- src/aiser.sh
+++ src/aiser.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 
-# Purpose:
-# Use ai to improve shell experience
-
-# default AI model:
-# ollama zephyr
+# Purpose: Use ai to improve shell experience
+# Default AI model: Ollama Zephyr
 
 ai_cmd="ollama run zephyr"
 
-ask_ollama(){
-    
-    fmessage=$1
-    fcontext=$2
-
-    cat $fmessage
-    #cat $fcontext
-    echo -e "\n--------------------------------------------------------------------------------\n"
-    #echo "Please answer in short: $input_message" | ollama run zephyr
-    cat $fmessage $fcontext | $ai_cmd
+ask_ollama() {
+  fmessage=$1
+  fcontext=$2
+  
+  cat $fmessage
+  #cat $fcontext
+  echo -e "\n--------------------------------------------------------------------------------\n"
+  #echo "Please answer in short: $input_message" | ollama run zephyr
+  cat $fmessage $fcontext | $ai_cmd
 }
 
-fcontext=`mktemp`
-fmessage=`mktemp`
+fcontext=$(mktemp)
+fmessage=$(mktemp)
 
-for words in $@
-do
-    if [ -f $words ];
-    then
-	echo -n "file $words " >> $fmessage
-	echo "File $words:" >> $fcontext
-	cat $words >> $fcontext
-	echo "" >> $fcontext
-    else
-	echo -n "$words " >> $fmessage
-    fi
+for words in "$@"; do
+  if [ -f $words ]; then
+    echo -n "File $words " >> $fmessage
+    echo "File $words:" >> $fcontext
+    cat $words >> $fcontext
+    echo "" >> $fcontext
+  else
+    echo -n "$words " >> $fmessage
+  fi
 done
 
 ask_ollama $fmessage $fcontext
@@ -41,4 +35,4 @@ ask_ollama $fmessage $fcontext
 rm $fcontext
 rm $fmessage
 
-#alias aa="ask_in_line $fcontext"
+# alias aa="ask_in_line $fcontext"
